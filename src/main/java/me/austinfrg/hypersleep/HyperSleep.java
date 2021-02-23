021af2536b33e449fd52cefc842d56543949f01f
AustinFRG <austin.cartee@yahoo.com>
2021-02-22 20:06:46 -0500

    Changed time to slowly change, added more configuration and added notes in config.

--- src/main/java/me/austinfrg/hypersleep/HyperSleep.java
+++ src/main/java/me/austinfrg/hypersleep/HyperSleep.java
@@ -29,23 +29,29 @@ public class HyperSleep extends JavaPlugin implements Listener {
         String player = e.getPlayer().getDisplayName();
         World world = e.getPlayer().getWorld();
         if (world.getTime() >= 13000) {
-            world.setTime(0);
+            Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
+                if (world.getTime() >= 13000) {
+                    world.setTime(world.getTime() + Integer.parseInt(Objects.requireNonNull(this.getConfig().getString("time-change-interval"))));
+                }
+            }, Integer.parseInt(Objects.requireNonNull(this.getConfig().getString("time-change-speed"))), Integer.parseInt(Objects.requireNonNull(this.getConfig().getString("time-change-speed"))));
             world.setThundering(false);
             world.setStorm(false);
-            e.setCancelled(Boolean.parseBoolean(this.getConfig().getString("stop-entering-bed")));
+            e.setCancelled(this.getConfig().getBoolean("stop-entering-bed"));
             Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(Objects.requireNonNull(this.getConfig().getString("time-changed-msg")).replace("%player%", player))));
         }
+        else if (world.hasStorm() || world.isThundering()) {
+            Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
+                if (world.getTime() >= 13000) {
+                    world.setTime(world.getTime() + Integer.parseInt(Objects.requireNonNull(this.getConfig().getString("time-change-interval"))));
+                }
+            }, Integer.parseInt(Objects.requireNonNull(this.getConfig().getString("time-change-speed"))), Integer.parseInt(Objects.requireNonNull(this.getConfig().getString("time-change-speed"))));
+            world.setThundering(false);
+            world.setStorm(false);
+            e.setCancelled(this.getConfig().getBoolean("stop-entering-bed"));
+            Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(Objects.requireNonNull(this.getConfig().getString("weather-changed-msg")).replace("%player%", player))));
+        }
         else {
-            if (world.hasStorm() || world.isThundering()) {
-                world.setTime(0);
-                world.setThundering(false);
-                world.setStorm(false);
-                e.setCancelled(Boolean.parseBoolean(this.getConfig().getString("stop-entering-bed")));
-                Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(Objects.requireNonNull(this.getConfig().getString("weather-changed-msg")).replace("%player%", player))));
-            }
-            else {
-                e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(Objects.requireNonNull(this.getConfig().getString("already-day-msg")).replace("%player%", player))));
-            }
+            e.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(Objects.requireNonNull(this.getConfig().getString("already-day-msg")).replace("%player%", player))));
         }
     }
 }
